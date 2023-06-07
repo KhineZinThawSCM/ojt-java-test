e12a56315b041887e0b7a114e1011d9c92a6f591
KhineZinThawSCM <scm.khinzin.thaw39@gmail.com>
2023-06-07 10:44:01 +0630

    create welcome file

---
+++ src/main/java/ojt/Hello.java
@@ -0,0 +1,8 @@
+package main.java.ojt;
+
+public class Hello {
+	
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author khinezinthaw
+ *
+ */
+module OJT {
+}
